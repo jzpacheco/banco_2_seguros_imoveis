001eea733c82ad65fca9ae925fcb89b0caa804d1
Jefferson Z. Pacheco <43594917+jzpacheco@users.noreply.github.com>
2023-12-05 18:15:42 -0300

    Update WebConfig.java

--- back-end/src/main/java/com/unisatc/crudbd2/config/WebConfig.java
+++ back-end/src/main/java/com/unisatc/crudbd2/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.unisatc.crudbd2.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.originPatterns:default}")
     private String corOriginPatterns ="";
 
     @Bean
